bce579933348d966c40115d2608cf5624f4dceb5
Verlamov <verlamov.m@gmail.com>
2017-03-25 17:57:18 +0200

    update HibernateConfig

--- src/main/java/com/meraki/configuration/HibernateConfig.java
+++ src/main/java/com/meraki/configuration/HibernateConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate4.HibernateTemplate;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
@@ -35,7 +36,7 @@ public class HibernateConfig {
     @Bean
     public SessionFactory sessionFactory() {
         LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
-        localSessionFactoryBean.setDataSource(dataSource());
+        localSessionFactoryBean.setDataSource(getDataSource());
         localSessionFactoryBean.setPackagesToScan("com.meraki");
         localSessionFactoryBean.setHibernateProperties(hibernateProperties());
         try {
@@ -47,30 +48,41 @@ public class HibernateConfig {
     }
 
 
-
-
-    @Bean(destroyMethod = "close")
-    public DataSource dataSource(){
-        HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/meraki_db");
-//        hikariConfig.setJdbcUrl("jdbc:mysql://10.103.185.145:3306/meraki_db");
-        hikariConfig.setUsername("root");
-        hikariConfig.setPassword("root");
-//        hikariConfig.setPassword("D120d10k83");
-
-        hikariConfig.setMaximumPoolSize(5);
-        hikariConfig.setConnectionTestQuery("SELECT 1");
-        hikariConfig.setPoolName("springHikariCP");
-
-        hikariConfig.addDataSourceProperty("dataSource.cachePrepStmts", "true");
-        hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSize", "250");
-        hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSqlLimit", "2048");
-        hikariConfig.addDataSourceProperty("dataSource.useServerPrepStmts", "true");
-
-        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+    @Bean
+    public DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(environment.getProperty("database.driver"));
+        dataSource.setUrl(environment.getProperty("database.url"));
+        dataSource.setUsername(environment.getProperty("database.root"));
+        dataSource.setPassword(environment.getProperty("database.password"));
         return dataSource;
     }
+
+
+
+//// heraki CONFIG
+//    @Bean(destroyMethod = "close")
+//    public DataSource dataSource(){
+//        HikariConfig hikariConfig = new HikariConfig();
+//        hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/meraki_db");
+////        hikariConfig.setJdbcUrl("jdbc:mysql://10.103.185.145:3306/meraki_db");
+//        hikariConfig.setUsername("root");
+//        hikariConfig.setPassword("root");
+////        hikariConfig.setPassword("D120d10k83");
+//
+//        hikariConfig.setMaximumPoolSize(5);
+//        hikariConfig.setConnectionTestQuery("SELECT 1");
+//        hikariConfig.setPoolName("springHikariCP");
+//
+//        hikariConfig.addDataSourceProperty("dataSource.cachePrepStmts", "true");
+//        hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSize", "250");
+//        hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSqlLimit", "2048");
+//        hikariConfig.addDataSourceProperty("dataSource.useServerPrepStmts", "true");
+//
+//        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+//        return dataSource;
+//    }
 
     @Bean
     public HibernateTransactionManager hibernateTransactionManager() {
