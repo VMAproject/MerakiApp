62651f5d9a26c9cb1b46c0b2c01bf8bb8afa516a
TsymbalenkoOleksandr <sashkoi1234@gmail.com>
2017-04-04 02:13:21 +0300

    adding description to faq

--- src/main/webapp/resources/js/faq.js
+++ src/main/webapp/resources/js/faq.js
@@ -11,4 +11,20 @@ function PopUpHide() {
 
 function PopUpToggle() {
     $('#popup1').toggle();
+    var vista = $("#WindowsVista");
+    var popup = $("#popup1");
+    if(vista.is((':visible'))) {
+        var topPos = vista.position().top;
+        setInterval(stickIt, 10);
+        function stickIt() {
+            var top = vista.offset().top;
+            if (top<=0) {
+                var scrlTop = popup.scrollTop();
+                vista.addClass('sticking');
+                if(topPos > scrlTop) {
+                    vista.removeClass('sticking');
+                }
+            }
+        }
+    }
 }
