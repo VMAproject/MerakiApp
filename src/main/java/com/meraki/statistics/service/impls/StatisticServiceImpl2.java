876543e9cdcb517180febe9395ea04ad12bd15c2
Rostyslav Matushkin <rm.cooperation@gmail.com>
2017-04-04 22:04:06 +0300

    fix compare Event to Store

--- src/main/java/com/meraki/statistics/service/impls/StatisticServiceImpl2.java
+++ src/main/java/com/meraki/statistics/service/impls/StatisticServiceImpl2.java
@@ -3,6 +3,7 @@ package com.meraki.statistics.service.impls;
 import com.meraki.entity.Event;
 import com.meraki.entity.Observation;
 import com.meraki.entity.Router;
+import com.meraki.entity.Store;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -28,16 +29,50 @@ public class StatisticServiceImpl2 {
     public List<Observation> getAllUniqueClientsByEventId(long id) {
         List<Observation> resultList = new ArrayList<>();
         Event event = getEventById(id);
-        List<Router> routersByEventId = getRoutersByEventId(id);
-        Long[] routersId = getRoutersId(routersByEventId);
+        Long[] routersId = getRoutersId(getRoutersByEventId(id));
 
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
-
         List<Observation> loadedList = (List<Observation>) session
                 .createSQLQuery("SELECT * FROM Observation WHERE rssi >= 15 AND (seenTime BETWEEN (:dateFrom) AND (:dateTo)) AND router_id IN (:routersId) GROUP BY clientMac;")
                 .addEntity(Observation.class)
                 .setParameterList("routersId", routersId)
+                .setParameter("dateFrom", event.getDateFrom())
+                .setParameter("dateTo", event.getDateTo())
+                .list();
+        transaction.commit();
+        session.close();
+
+        resultList.addAll(loadedList);
+
+        return resultList;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Observation> compareEventToStore(long eventId, long storeId) {
+        List<Observation> resultList = new ArrayList<>();
+        Event event = getEventById(eventId);
+        Long[] eventRoutersId = getRoutersId(getRoutersByEventId(eventId));
+        Long[] storeRoutersId = getRoutersId(getRoutersByStoreId(storeId));
+
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        List<Observation> loadedList = session
+                .createSQLQuery("SELECT *\n" +
+                "FROM Observation\n" +
+                "WHERE\n" +
+                "  clientMac IN (SELECT clientMac\n" +
+                "                FROM observation\n" +
+                "                WHERE rssi >= 15 AND (seenTime BETWEEN (:dateFrom) AND (:dateTo)) AND router_id IN (:eventRoutersId)\n" +
+                "                GROUP BY clientMac)\n" +
+                "  AND clientMac IN (SELECT clientMac\n" +
+                "                    FROM observation\n" +
+                "                    WHERE rssi >= 15 AND seenTime >= (:dateFrom) AND router_id IN (:storeRoutersId)\n" +
+                "                    GROUP BY clientMac)\n" +
+                "GROUP BY clientMac")
+                .addEntity(Observation.class)
+                .setParameterList("eventRoutersId", eventRoutersId)
+                .setParameterList("storeRoutersId", storeRoutersId)
                 .setParameter("dateFrom", event.getDateFrom())
                 .setParameter("dateTo", event.getDateTo())
                 .list();
@@ -73,6 +108,19 @@ public class StatisticServiceImpl2 {
         return routers;
     }
 
+    @SuppressWarnings("unchecked")
+    private List<Router> getRoutersByStoreId(long id) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        List<Router> routers = (List<Router>) session.createQuery("from Router where store_id = (:id)")
+                .setParameter("id", id)
+                .list();
+        transaction.commit();
+        session.close();
+
+        return routers;
+    }
+
     private Event getEventById(long id) {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
@@ -83,4 +131,13 @@ public class StatisticServiceImpl2 {
         return event;
     }
 
+    private Store getStoreById(long id) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Store store = (Store) session.createQuery("from Store where store_id = (:id)").setParameter("id", id).uniqueResult();
+        transaction.commit();
+        session.close();
+
+        return store;
+    }
 }
